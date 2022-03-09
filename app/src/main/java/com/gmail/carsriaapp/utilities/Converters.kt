0fa36b3173c3906465e324be0f39ac9f27faa9e1
bukin26 <mg.bukin26@gmail.com>
2022-03-09 17:50:48 +0200

    added room converters

---
+++ app/src/main/java/com/gmail/carsriaapp/utilities/Converters.kt
@@ -0,0 +1,14 @@
+package com.gmail.carsriaapp.utilities
+
+import androidx.room.TypeConverter
+
+class Converters {
+    @TypeConverter
+    fun toListOfStrings(flatStringList: String): List<String> {
+        return flatStringList.split(",")
+    }
+    @TypeConverter
+    fun fromListOfStrings(listOfString: List<String>): String {
+        return listOfString.joinToString(",")
+    }
+}
