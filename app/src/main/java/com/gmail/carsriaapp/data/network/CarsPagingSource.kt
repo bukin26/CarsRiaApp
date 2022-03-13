85b7607c85297cbb4e883087db7c3859f96a395b
bukin26 <mg.bukin26@gmail.com>
2022-03-13 16:02:54 +0200

    added CarsPagingSource

--- app/src/main/java/com/gmail/carsriaapp/data/network/CarsPagingSource.kt
+++ app/src/main/java/com/gmail/carsriaapp/data/network/CarsPagingSource.kt
@@ -1,4 +1,47 @@
 package com.gmail.carsriaapp.data.network
 
-class CarsPagingSource {
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.gmail.carsriaapp.data.entity.CarInfo
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+
+class CarsPagingSource @AssistedInject constructor(
+    private val carService: CarService,
+    @Assisted private val idList: List<String>
+) : PagingSource<Int, CarInfo>() {
+    override fun getRefreshKey(state: PagingState<Int, CarInfo>): Int? {
+        return null
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, CarInfo> {
+
+        val pageIndex = params.key ?: 0
+
+        val cars = mutableListOf<CarInfo>()
+
+        idList.forEachIndexed { index, id ->
+            if (index >= pageIndex && index < (pageIndex + params.loadSize)) {
+                cars.add(carService.getCar(id).body() ?: CarInfo())
+            }
+
+        }
+
+        return try {
+
+            return LoadResult.Page(
+                data = cars,
+                prevKey = if (pageIndex == 0) null else pageIndex - params.loadSize,
+                nextKey = if (cars.size == params.loadSize) pageIndex + params.loadSize + 1 else null
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    @AssistedFactory
+    interface Factory {
+        fun create(@Assisted idList: List<String>): CarsPagingSource
+    }
 }
